3abfb0a0859ad04f4db1d51b768dfd9afb4d37da
pratm1304 <121380335+pratm1304@users.noreply.github.com>
2023-10-27 20:06:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let window_width = window.innerWidth;
-let projects_box = document.querySelector(".projects");
-if(window_width<515){
-    projects_box.style.marginTop = "25rem";
+
+
+
+function checkScreenSize(){
+    
+    let projbox = document.querySelector(".project");
+    if(window.innerWidth<520){
+        projbox.style.marginTop = "50rem";
+    }
 }
+
+window.addEventListener("load", checkScreenSize);
+window.addEventListener("resize", checkScreenSize);
